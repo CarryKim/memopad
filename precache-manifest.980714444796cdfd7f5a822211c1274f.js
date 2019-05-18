12cb67cf733822dee7ee6c1b05683ca59d4225d2
Minji Kim <minjikim1126@hotmail.com>
2019-05-18 13:37:35 +0900

    Updates

---
+++ precache-manifest.980714444796cdfd7f5a822211c1274f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "232975de1da1b0ef4b5f5692c42614b8",
+    "url": "/memopad/index.html"
+  },
+  {
+    "revision": "fb34359ef4af9938a611",
+    "url": "/memopad/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "7c1e8cde0f1f57d059dd",
+    "url": "/memopad/static/js/2.7e675ef3.chunk.js"
+  },
+  {
+    "revision": "fb34359ef4af9938a611",
+    "url": "/memopad/static/js/main.1c4a98ff.chunk.js"
+  },
+  {
+    "revision": "ef477654ee95326af431",
+    "url": "/memopad/static/js/runtime~main.bd74dc90.js"
+  }
+]);
